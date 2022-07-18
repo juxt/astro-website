733472b8784fe4704971af6a7a4dd40578cce939
Lucio D'Alessandro <lda@juxt.pro>
2022-07-19 01:54:28 +0200

    Add gen astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import preact from '@astrojs/preact';
-
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), preact()]
-});
+})
