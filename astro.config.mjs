2c319ea1cc5c1c9eea7e9a754677bd045acab642
Lucio D'Alessandro <lda@juxt.pro>
2022-07-31 00:19:52 +0200

    Add more homepage sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import preact from '@astrojs/preact'
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), preact()],
+  server: { port: 3000, host: '0.0.0.0' },
   site: 'https://juxt-site-v3-main.vercel.app'
 })
