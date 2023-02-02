31ea26ca515115d65ad382a3ace7522f34bd88a9
IAmNini <fortuny.carrega@gmail.com>
2023-02-02 11:36:55 +0000

    Add new fontsizes + change fontsize and line height in cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,6 +22,21 @@ module.exports = {
           }
         }
       }
+    },
+    fontSize: {
+      xxs: '0.579rem',
+      xs: '0.694rem',
+      sm: '0.833rem',
+      base: '1rem',
+      md: '1.2rem',
+      lg: '1.44rem',
+      xl: '1.728rem',
+      '2xl': '2.074rem',
+      '3xl': '2.488rem',
+      '4xl': '2.986rem',
+      '5xl': '3.583rem',
+      '6xl': '4.3rem',
+      '7xl': '5.16rem'
     }
   },
   plugins: [require('@tailwindcss/typography')]
