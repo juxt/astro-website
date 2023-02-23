97e4484caaaf34768876efea9b691d45821edc73
Luigia D'Alessandro <luigiadalessandro22@gmail.com>
2023-02-23 15:42:17 +0100

    edit clojure-in hp

--- src/components/ClojureInBlogs.tsx
+++ src/components/ClojureInBlogs.tsx
@@ -26,8 +26,8 @@ export function ClojureInBlogs({
             <div
               onClick={() => setSelectedIndustry(value)}
               className={classNames(
-                'border-2 p-2 border-green-900 cursor-pointer',
-                { 'bg-purple-700': selectedIndustry === value }
+                'border-2 p-2 border-green-500 cursor-pointer',
+                { 'bg-green-500': selectedIndustry === value }
               )}
             >
               {label}
@@ -37,26 +37,23 @@ export function ClojureInBlogs({
       </div>
 
       <div className='flex flex-col gap-28'>
-        <div className='flex flex-col items-center'>
-          <div className='grid lg:grid-cols-[repeat(2,_1fr)] xl:grid-cols-[repeat(3,_1fr)] justify-center gap-x-6 gap-y-20 py-12'>
-            {blogs?.map((blog) => {
+        <div className='flex flex-col'>
+          <div className='grid lg:grid-cols-[repeat(2,_1fr)] xl:grid-cols-[repeat(3,_1fr)] gap-x-14 gap-y-14 py-12'>
+            {blogs.map((blog) => {
+              const [location, title] = blog.title.split(':')
               return (
                 <a
                   href={`/blog/${blog.href}`}
-                  className='group cursor-pointer justify-between flex flex-col w-96 mx-auto gap-6'
+                  className='group cursor-pointer justify-end flex flex-col w-80 mx-auto gap-4'
                 >
-                  <div className='text-white flex flex-col justify-between h-24'>
-                    <h3 className='text-2xl font-semibold'>{blog.title}</h3>
-                    <h5 className='text-lg font-extralight'>
-                      {blog.description}
-                    </h5>
+                  <div className='text-white flex flex-col justify-between'>
+                    <h3 className='text-lg font-semibold'>
+                      {location}: <br />
+                      {title}
+                    </h3>
+                    <h5 className='font-extralight'>{blog.description}</h5>
                   </div>
                   <img className='' alt='blog' src={blog.heroImage} />
-                  <div>
-                    <p className='text-lg font-light text-white'>
-                      {blog.publishedDate}
-                    </p>
-                  </div>
                 </a>
               )
             })}
