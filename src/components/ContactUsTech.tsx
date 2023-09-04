5cb7e65c9e64b38aad1580d865bdb35ecff827f4
Luigia D'Alessandro <luigiadalessandro22@gmail.com>
2023-09-04 16:20:39 +0200

    create tech-roundtable page

---
+++ src/components/ContactUsTech.tsx
@@ -0,0 +1,242 @@
+import { useState } from 'preact/hooks'
+import { useForm } from 'react-hook-form'
+import jdt from '@assets/people/jdt.jpg'
+import { TickIcon } from '@components/Icons'
+
+const inputProps = {
+  type: 'text',
+  className:
+    'flex-grow-1 bg-gray-50 border border-juxt text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-2.5'
+}
+
+function TickBox({ text }: { text: string }) {
+  return (
+    <div className='flex gap-4 items-center'>
+      <div className='bg-juxt w-8 h-8 shrink-0 border border-white hover:shadow-lg'>
+        <TickIcon className={'fill-white'} />
+      </div>
+      <p className='text-lg'>{text}</p>
+    </div>
+  )
+}
+
+const techLogos = [
+  'kotlin.svg',
+  'iceberg.svg',
+  'apache-arrow.png',
+  'kafka.png',
+  'kubernetes.png',
+  'trino.png',
+  'duckDB.png',
+  'parquet.png',
+  'clickhouse.svg'
+]
+
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful }
+  } = useForm()
+
+  const [isSubmitError, setIsSubmitError] = useState<boolean>(false)
+
+  function submitContactForm(data) {
+    const json = JSON.stringify(data)
+
+    return fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: json
+    }).catch((error) => {
+      console.error(error)
+      setIsSubmitError(true)
+    })
+  }
+
+  const hasErrors = Object.keys(errors).length > 0
+
+  const isSubmittingComponent = (
+    <div className='md:px-8 text-black text-xl md:text-2xl'>
+      Submitting your form...
+    </div>
+  )
+
+  const isSubmitSuccessfulComponent = (
+    <div className='md:px-8 text-center text-xl md:text-2xl text-juxt'>
+      Thanks for your submission! We'll be in touch shortly.
+    </div>
+  )
+
+  const isSubmitErrorComponent = (
+    <div className='md:px-8 text-xl md:text-2xl text-red-600'>
+      Mmm.. It seems there's a problem.. Anyways, you can directly reach out to
+      Jeremy at <strong>jdt@juxt.pro </strong>
+    </div>
+  )
+
+  return (
+    <>
+      {isSubmitting ? (
+        isSubmittingComponent
+      ) : isSubmitError ? (
+        isSubmitErrorComponent
+      ) : isSubmitSuccessful ? (
+        isSubmitSuccessfulComponent
+      ) : (
+        <form
+          onSubmit={handleSubmit(submitContactForm)}
+          id='form'
+          className='flex flex-col gap-4'
+        >
+          <input
+            type='hidden'
+            {...register('subject')}
+            value='New Submission from Better Call Joe'
+          />
+          <input
+            type='hidden'
+            {...register('access_key')}
+            value='c2bf653a-2baa-466d-bbcc-390272663918'
+          />
+          <div className='flex flex-col md:flex-row gap-4'>
+            <input
+              {...inputProps}
+              {...register('firstName', { required: true })}
+              placeholder='First Name'
+            />
+            <input
+              {...inputProps}
+              {...register('lastName', { required: true })}
+              placeholder='Last Name'
+            />
+          </div>
+          <input
+            {...inputProps}
+            {...register('email', { required: true })}
+            placeholder='Work Email'
+            type='email'
+          />
+          <div className='flex flex-col md:flex-row gap-4'>
+            <input
+              {...inputProps}
+              {...register('jobTitle', { required: true })}
+              placeholder='Job Title'
+            />
+            <input
+              {...inputProps}
+              {...register('companyName', { required: true })}
+              placeholder='Company Name'
+            />
+          </div>
+          <div className='flex flex-col md:flex-row gap-4'>
+            <input
+              {...inputProps}
+              {...register('phone')}
+              placeholder='Phone (Optional)'
+            />
+            <input
+              {...inputProps}
+              {...register('country', { required: true })}
+              placeholder='Country'
+            />
+          </div>
+          <div className='text-xs'>
+            By submitting your details you agree to JUXT’s{' '}
+            <a href='/privacy-policy' target='_blank' className='underline'>
+              Privacy Policy
+            </a>
+          </div>
+
+          {hasErrors && (
+            <div className='text-red-500'>
+              All fields but 'Phone' are required
+            </div>
+          )}
+
+          <button
+            type='submit'
+            className='bg-juxt px-4 py-3 text-white hover:text-zinc-800 font-bold hover:shadow-lg visited:text-white active:text-white text-md rounded-sm'
+          >
+            Send Form
+          </button>
+          <div className='flex flex-row gap-6 text-xs items-center max-w-[350px]'>
+            <div className='flex flex-col gap-2 leading-snug'>
+              What happens after you hit “send”? Jeremy will be in touch with you
+              to secure a call within 48 hours.
+            </div>
+          </div>
+        </form>
+      )}
+    </>
+  )
+}
+
+export default function ContactUs() {
+  return (
+    <div className='min-h-screen flex justify-center bg-zinc-800'>
+      <div className='p-8 py-8 px-8 lg:py-14 lg:px-16 max-w-7xl'>
+        <a href='/' className='block pb-8'>
+          <img width='100' src='/images/logo-on-dark.svg' alt='Juxt Logo' />
+        </a>
+        <div className='flex flex-col lg:justify-between lg:flex-row gap-3 pb-14'>
+          <div className='flex flex-col lg:gap-6 lg:w-2/3 gap-8'>
+            <div className='text-3xl md:text-4xl font-bold leading-snug text-left text-white uppercase'>
+              Technology Roundtable{' '}
+            </div>
+            <div className='text-medium md:text-2xl text-white leading-snug uppercase'>
+              Dedicated event with Jeremy Taylor - Head of Product, XTDB
+            </div>
+          </div>
+
+          <div className='flex items-center lg:items-stretch flex-col gap-4'>
+            <img src={jdt.src} alt='Jdt' width='150' />
+            <div className='text-sm text-left font-light text-juxt'>
+              Jeremy Taylor <br /> JUXT Head of Production XTDB
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col-reverse lg:flex-row bg-zinc-800 gap-16'>
+          <div className='flex flex-col gap-10 text-left lg:w-1/2'>
+            <div className='text-lg text-white md-text-3xl font-light leading-snug'>
+              Looking to explore new tech with your team? JUXT wants to take you on a tour through some of the latest open source software innovations that are relevant for you!
+            </div>
+            <div className='text-lg text-white md-text-3xl font-light leading-snug'>
+              In as little as one hour, either virtually or at your offices, let Jeremy get you up-to-speed on new technologies that can and probably will impact your work in your near future. 
+            </div>
+            <div className='flex flex-col gap-10 text-white'>
+              <TickBox text='Discover little-known challenges in the development porcess - and discuss potential suitable solutions' />
+              <TickBox text='Formulate initial project requirements, timeline, and budgets' />
+              <TickBox text='Assess your overall fit with JUXT methodology and processes' />
+            </div>
+          </div>
+          <div className='flex flex-col lg:w-1/2 bg-white py-6 px-2 md:px-0'>
+            <div className='flex flex-col items-center gap-8'>
+              <div className='text-black text-2xl md:text-3xl font-bold'>
+                Book your free consultation
+              </div>
+              <Form />
+            </div>
+          </div>
+        </div>
+        <div className='text-xl text-center text-white leading-snug py-16'>
+          A few of our favourite trending logos:
+        </div>
+        <div className='py-6 gap-6'>
+          <div className='flex flex-wrap justify-center gap-4 lg:w-1/2 mx-auto'>
+            {techLogos.map((logo) => (
+              <img
+                src={`/images/tech-logos/${logo}`}
+                alt=''
+                className='w-24 md:w-20 flex-shrink-0'
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
