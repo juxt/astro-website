9192c96249f79f0544012c2b6b9f8958c955b088
Oliver Marshall <Akeboshiwind@users.noreply.github.com>
2023-11-01 17:24:50 +0000

    fix: change to using padding instead of margin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,7 +68,7 @@ export default function Navbar({ navLinks, enableDarkMode, navbarNoBg }) {
        * - No specific window size
        * On scrolling down and then back up the navbar would go an opaque off white.
       */
-      style="margin-top: 1px;"
+      style="padding-top: 1px;"
     >
       <div className='container mx-auto px-4 sm:px-12 2xl:px-0 max-w-7xl flex flex-wrap items-center justify-between h-full'>
         <a href='/' class='flex items-center w-20 z-10'>
