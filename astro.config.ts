73b96c37cfac53d27fa3bd3462741138ead907b4
Oliver Marshall <olivershawmarshall@gmail.com>
2023-11-30 17:53:45 +0000

    fix: don't use image passthrough

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
-import { defineConfig, passthroughImageService } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import { h } from 'hastscript'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
@@ -15,9 +15,6 @@ export default defineConfig({
     mdx(),
     preact(),
   ],
-  image: {
-      service: passthroughImageService(),
-  },
   site: 'https://juxt.pro/',
   markdown: {
     shikiConfig: {
