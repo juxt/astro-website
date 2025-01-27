5cb121f203bb53059acbf21d32dd58452d89b584
Yensid10 <ben.shearlock@gmail.com>
2025-01-27 09:39:19 +0000

    Created Page for XT25, added Alert Banner
    I copied the pages for XT24 and have adjusted them for XT25. This involved giving it a new URL (root/xt25), changing the title, removing agenda & speakers (commented out), changing the logo displayed, font & formatting adjustments, adjusting old informational content and fixing an issue with the contact us form. I have also enabled the Alert Banner, which will now redirect to the XT25 page.

--- src/components/ContactUsFormXT24.tsx
+++ src/components/ContactUsFormXT24.tsx
@@ -2,148 +2,153 @@ import { useState } from 'preact/hooks'
 import { useForm } from 'react-hook-form'
 
 const inputProps = {
-    type: 'text',
-    className:
-        'flex-grow-1 bg-gray-50 border border-juxt text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-2.5'
+  type: 'text',
+  className:
+    'flex-grow-1 bg-gray-50 border border-juxt text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-2.5'
 }
 
 export default function ContactUsFormXT24(props) {
-    const {
-        id = "form",
-        subject = "New Contact Us Submission",
-    } = props;
+  const { id = 'form', subject = 'New Contact Us Submission' } = props
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting, isSubmitSuccessful }
-    } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful }
+  } = useForm()
 
-    const [isSubmitError, setIsSubmitError] = useState<boolean>(false)
+  const [isSubmitError, setIsSubmitError] = useState<boolean>(false)
 
-    const isSubmitted = isSubmitSuccessful || isSubmitError;
+  const isSubmitted = isSubmitSuccessful || isSubmitError
 
-    function submitContactForm(data) {
-        const json = JSON.stringify(data)
+  function submitContactForm(data) {
+    const json = JSON.stringify(data)
 
-        return fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-            body: json
-        }).catch((error) => {
-            console.error(error)
-            setIsSubmitError(true)
-        })
-    }
+    return fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: json
+    }).catch((error) => {
+      console.error(error)
+      setIsSubmitError(true)
+    })
+  }
 
-    const hasErrors = Object.keys(errors).length > 0
+  const hasErrors = Object.keys(errors).length > 0
 
-    const isSubmitSuccessfulComponent = (
-        <div className='pt-10 text-center text-xl md:text-xl text-juxt'>
-            Thank you for your interest in XT24!
+  const isSubmitSuccessfulComponent = (
+    <div className='pt-10 text-center text-xl md:text-xl text-juxt'>
+      Thank you for your interest in XT25!
+      <br />
+      We'll reach out if spots become available.
+      <br />
+      <br />
+      <b class='bold'>
+        Please note: <br /> this is not a ticket confirmation
+      </b>
+    </div>
+  )
+
+  const isSubmitErrorComponent = (
+    <div className='md:px-8 text-xl md:text-2xl text-red-600'>
+      Mmm.. It seems there's a problem.. Anyways, you can reach us directly at{' '}
+      <strong>info@juxt.pro</strong>
+    </div>
+  )
+
+  return (
+    <>
+      {isSubmitSuccessful && !isSubmitError && isSubmitSuccessfulComponent}
+      {isSubmitError && isSubmitErrorComponent}
+
+      {!isSubmitted && (
+        <div className='text-center'>
+          <span className='uppercase text-center'>
+            Interested in Receiving an Invite?
             <br />
-            We'll reach out if spots become available.
-            <br />
-            <br />
-            <b class="bold">Please note: <br /> this is not a ticket confirmation</b>
+            Leave us your details, and we'll get back to you.
+          </span>
         </div>
-    )
+      )}
 
-    const isSubmitErrorComponent = (
-        <div className='md:px-8 text-xl md:text-2xl text-red-600'>
-            Mmm.. It seems there's a problem.. Anyways, you can reach us directly at <strong>info@juxt.pro</strong>
+      <form
+        onSubmit={handleSubmit(submitContactForm)}
+        id={id}
+        class={'flex flex-col gap-4 ' + (isSubmitted ? 'hidden' : '')}
+      >
+        <input type='hidden' {...register('subject')} value={subject} />
+        <input
+          type='hidden'
+          {...register('access_key')}
+          value='c2bf653a-2baa-466d-bbcc-390272663918'
+        />
+
+        <div className='flex flex-row gap-4'>
+          <input
+            {...inputProps}
+            {...register('firstName', { required: true })}
+            placeholder='First Name'
+          />
+          <input
+            {...inputProps}
+            {...register('lastName', { required: true })}
+            placeholder='Last Name'
+          />
         </div>
-    )
 
-    return (
-        <>
-            {isSubmitSuccessful && !isSubmitError && isSubmitSuccessfulComponent}
-            {isSubmitError && isSubmitErrorComponent}
-            <form
-                onSubmit={handleSubmit(submitContactForm)}
-                id={id}
-                class={'flex flex-col gap-4 ' + (isSubmitted ? 'hidden' : '')}
-            >
-                <input
-                    type='hidden'
-                    {...register('subject')}
-                    value={subject}
-                />
-                <input
-                    type='hidden'
-                    {...register('access_key')}
-                    value='c2bf653a-2baa-466d-bbcc-390272663918'
-                />
+        <input
+          {...inputProps}
+          {...register('email', { required: true })}
+          placeholder='Work Email'
+          type='email'
+        />
 
-                <div class="flex flex-row gap-4">
-                    <input
-                        {...inputProps}
-                        {...register('firstName', { required: true })}
-                        placeholder='First Name'
-                    />
-                    <input
-                        {...inputProps}
-                        {...register('lastName', { required: true })}
-                        placeholder='Last Name'
-                    />
-                </div>
+        <div className='flex flex-row gap-4'>
+          <input
+            {...inputProps}
+            {...register('jobTitle', { required: true })}
+            placeholder='Job Title'
+          />
+          <input
+            {...inputProps}
+            {...register('company', { required: true })}
+            placeholder='Company'
+          />
+        </div>
 
-                <input
-                    {...inputProps}
-                    {...register('email', { required: true })}
-                    placeholder='Work Email'
-                    type='email'
-                />
+        <div className='flex flex-row gap-4'>
+          <input
+            {...inputProps}
+            {...register('phone')}
+            placeholder='Phone (optional)'
+          />
+          <input
+            {...inputProps}
+            {...register('country', { required: true })}
+            placeholder='Country'
+          />
+        </div>
 
-                <div class="flex flex-row gap-4">
-                    <input
-                        {...inputProps}
-                        {...register('jobTitle', { required: true })}
-                        placeholder='Job Title'
-                    />
-                    <input
-                        {...inputProps}
-                        {...register('company', { required: true })}
-                        placeholder='Company'
-                    />
-                </div>
+        <div className='text-xs font-light'>
+          By submitting your details you agree to JUXT’s{' '}
+          <a href='/privacy-policy' target='_blank' className='underline'>
+            Privacy Policy
+          </a>
+        </div>
 
-                <div class="flex flex-row gap-4">
-                    <input
-                        {...inputProps}
-                        {...register('phone')}
-                        placeholder='Phone (optional)'
-                    />
-                    <input
-                        {...inputProps}
-                        {...register('country', { required: true })}
-                        placeholder='Country'
-                    />
-                </div>
+        {hasErrors && (
+          <div className='text-red-500'>All fields are required</div>
+        )}
 
-                <div className='text-xs font-light'>
-                    By submitting your details you agree to JUXT’s{' '}
-                    <a href='/privacy-policy' target='_blank' className='underline'>
-                        Privacy Policy
-                    </a>
-                </div>
-
-                {hasErrors && (
-                    <div className='text-red-500'>
-                        All fields are required
-                    </div>
-                )}
-
-                <input
-                    type='submit'
-                    value="Submit"
-                    disabled={isSubmitting}
-                    class='bg-juxt px-4 py-3 text-white hover:text-zinc-800 font-bold hover:shadow-lg visited:text-white active:text-white text-md rounded-sm cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed'
-                />
-            </form>
-        </>
-    )
+        <input
+          type='submit'
+          value='Submit'
+          disabled={isSubmitting}
+          className='bg-juxt px-4 py-3 text-white hover:text-zinc-800 font-bold hover:shadow-lg visited:text-white active:text-white text-md rounded-sm cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed'
+        />
+      </form>
+    </>
+  )
 }
